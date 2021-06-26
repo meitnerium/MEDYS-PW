6abcd219d9094da3515c2c827783d16fa8989e86
François Dion <meitnerium109@gmail.com>
2021-06-26 12:40:59 -0400

    Update orb_new.py

--- tools/orb_new.py
+++ tools/orb_new.py
@@ -13,20 +13,21 @@ class molecule():
 
 
 class basis():
-    def __init__(self, listecons, l):
+    def __init__(self, listecons, l, ao=[100]):
         self.listecons = listecons
         self.l = l
+        self.ao = ao
 
 
 class atom():
-    def __init__(self,   basis = [], pos=[0.,0.,0.], chg=1.0, atype='H'):
+    def __init__(self,   bases = [], pos=[0.,0.,0.], chg=1.0, atype='H'):
         self.atype = atype
         self.chg = chg
         self.pos = pos
-        self.basis = basis
+        self.bases = bases
 
     def add_basis(self,basis):
-        self.basis.append(basis)
+        self.bases.append(basis)
 REP = "../input/CO2/800nm_I5E13_NX_200_1MO_2e/BASIS/CO2/1/"
 xmax=6
 zmax=6
@@ -35,23 +36,6 @@ x=np.arange(-xmax,xmax,0.01)
 z=np.arange(-zmax,zmax,0.01)
 
 xv, zv = np.meshgrid(x, z, sparse=False, indexing='ij')
-
-mocoeff=np.zeros((27,27))
-fname=REP+'mocoef_scf.sp'
-with open(fname) as f:
-    content = f.readlines()
-content = [x.strip() for x in content]
-norb=0
-morb=0
-ncont=10
-while morb<27:
- while norb<27:
-  for number in content[ncont].split():
-    mocoeff[morb][norb]=number
-    norb=norb+1
-  ncont=ncont+1
- morb=morb+1
- norb=0
 
 def get_orb(argosls, line):
     if "orbitals" in argosls[line]:
@@ -81,6 +65,7 @@ def get_orb(argosls, line):
         
 f = open(REP+'argosls.sp', "r")
 argosls = f.readlines()
+f.close()
 # title = argosls[2]
 #print(str(mocoeff[1]))
 #print("!!!!!!!!!!!!! TTTTESSSSSSSSSSSSTTTTTTT")
@@ -88,8 +73,32 @@ argosls = f.readlines()
 line=3
 NGEN, NS, NAORDS, NCONS, NGCS, ITOL, ICUT, AOINTS, ONLY1E, \
  INRM, NCRS, L1REC, L2REC, AOINT2, FSPLIT = argosls[line].split()
- 
- 
+
+NAORDS = int(NAORDS)
+
+fname=REP+'mocoef_scf.sp'
+with open(fname) as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+nmocoeffs = int(content[6].split()[0])
+
+print(nmocoeffs)
+mocoeff = np.zeros((nmocoeffs, nmocoeffs))
+norb=0
+morb=0
+ncont=10
+while morb < nmocoeffs:
+#while morb<27:
+    while norb < nmocoeffs:
+        for number in content[ncont].split():
+            print(morb,norb,number)
+            mocoeff[morb][norb] = number
+            norb = norb + 1
+        ncont = ncont + 1
+    morb=morb+1
+    norb=0
+
+f.close()
 while not ("nuclear repulsion energy" in argosls[line]):
     line = line + 1
 
@@ -103,7 +112,7 @@ atome = atom()
 def searchorb(atome,argosls,line,mol):
     if "orbitals" in argosls[line]:
         line,orb,TYPE,ao, l = get_orb(argosls, line)
-        atome.add_basis(basis(listecons = orb, l=l))
+        atome.add_basis(basis(listecons=orb, l=l, ao=ao))
         print(orb)
         print("line = ", line)
         print(argosls[line])
@@ -133,8 +142,8 @@ def searchatomorb(argosls,line,atome,mol):
             print(ATOMNUM)
             print(X,Y,Z)
             
-            atome = atom(atype=ATYPE, pos = [X,Y,Z], chg = CHG, basis=[])
-            print("test new basis", atome.basis )
+            atome = atom(atype=ATYPE, pos = [X,Y,Z], chg = CHG, bases=[])
+            print("test new basis", atome.bases )
             line = line + 2
             searchorb(atome,argosls,line,mol)
             
@@ -157,62 +166,55 @@ for n in range(len(mol.atoms)):
     atome = mol.atoms[n]
     print(atome.atype)
     print(atome.pos)
-    for base in atome.basis:
+    for base in atome.bases:
 
         print(base.listecons)
         if base.l == 0:
             orb = np.zeros(xv.shape)
             for con in base.listecons:
                 orb = orb + con[1] * np.exp(-con[0] * ((xv-atome.pos[0]) ** 2.0 + (zv-atome.pos[2]) ** 2.0))
-        if base.l == 1:
-            if p == 0:
-                orb = np.zeros(xv.shape)
-                for con in base.listecons:
-                    orb = orb + con[1] * np.exp(-con[0] * ((xv - atome.pos[0]) ** 2.0 + (zv - atome.pos[2]) ** 2.0))
-                orb = orb * xv
-                p = 1
-                plt.contourf(xv, zv, orb)
-                plt.colorbar()
-                plt.savefig('aoorb_' + str(m) + '.png')
-                plt.close()
-                aoorbs.append(orb)
-                m = m + 1
-            if p == 1:
-                orb = np.zeros(xv.shape)
-                for con in base.listecons:
-                    orb = orb + con[1] * np.exp(-con[0] * ((xv - atome.pos[0]) ** 2.0 + (zv - atome.pos[2]) ** 2.0))
-                orb = orb * 0.
-                plt.contourf(xv, zv, orb)
-                plt.colorbar()
-                plt.savefig('aoorb_' + str(m) + '.png')
-                plt.close()
-                aoorbs.append(orb)
-                m = m + 1
-                p = 2
-            if p == 2:
-                orb = np.zeros(xv.shape)
-                for con in base.listecons:
-                    orb = orb + con[1] * np.exp(-con[0] * ((xv - atome.pos[0]) ** 2.0 + (zv - atome.pos[2]) ** 2.0))
-                orb = orb * zv
+            aoorbs.append(orb)
 
-                p = 0
+            plt.contourf(xv, zv, orb)
+            plt.colorbar()
+            plt.savefig('aoorb_' + str(m) + '.png')
+            plt.close()
+            m = m + 1
+        if base.l == 1:
+            for iao in base.ao:
+                if iao == "100":
+                    mult = xv
+                elif iao == "010":
+                    mult = 0
+                elif iao == "001":
+                    mult = zv
+                orb = np.zeros(xv.shape)
+                for con in base.listecons:
+                    orb = orb + con[1] * np.exp(-con[0] * ((xv - atome.pos[0]) ** 2.0 + (zv - atome.pos[2]) ** 2.0))
+                orb = orb * mult
+                plt.contourf(xv, zv, orb)
+                plt.colorbar()
+                plt.savefig('aoorb_' + str(m) + '.png')
+                plt.close()
+                aoorbs.append(orb)
+                m = m + 1
+
         #print('Min, Max')
         #print(min(orb))
         #print(max(orb))
-        aoorbs.append(orb)
 
-        plt.contourf(xv, zv, orb)
-        plt.colorbar()
-        plt.savefig('aoorb_'+str(m)+'.png')
-        plt.close()
-        m = m + 1
 moorb = []
 #print("printing moceoeoof")
 m = 0
 for mocoefs in mocoeff:
     orb = np.zeros(xv.shape)
+    print("MOCEOF (",m,") = ",mocoefs)
+    print("Sum**2 = ",np.sum(mocoefs)**2)
+    norm = 0
     for mocoef in mocoefs:
         orb = orb + float(mocoef)*aoorbs[m]
+        norm = norm + mocoef**2
+    print("norm =" , norm)
     plt.contourf(xv, zv, orb)
     plt.colorbar()
     plt.savefig('moorb_' + str(m) + '.png')
