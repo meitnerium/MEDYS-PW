71a7810112961a14ef6652b805894a53aa3aa99d
fdion2@chm.ulaval.ca <Francois.Dion@drdc-rddc.gc.ca>
2021-12-15 10:32:13 -0600

    some error in convert.py

--- tools/analyse.py
+++ tools/analyse.py
@@ -1,1 +1,145 @@
 import matplotlib.pyplot as plt
+def tick_function(X):
+        V = X*0.0241888425
+        return ["%.0f" % z for z in V]
+
+def au2fs(X):
+        V = X*0.0241888425
+        return V
+
+def fs2au(X):
+        V = X/0.0241888425
+        return V
+xlabeltime="Time (u.a.)"
+ylabelchamp="Intensity (u.a.)"
+ylabelpop="Population"
+xlabelspectre="k"
+ylabelspectre="Intensity"
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline 
+
+champ=np.loadtxt( 'champ.dat')
+ax1 = plt.subplot(1, 1, 1)
+ax1.plot(champ[:,0],champ[:,2],"r-")
+plt.xlabel(xlabeltime)
+plt.ylabel(ylabelchamp)
+ax1R = ax1.twiny()
+arrayenfs=np.arange(0,20,5)
+print(arrayenfs)
+arrayenua=fs2au(arrayenfs)
+print(arrayenua)
+new_tick_locations=arrayenfs
+new_tick_locations=np.array([200])
+ax1R.set_xlim(ax1.get_xlim())
+ax1R.set_xticks(arrayenua)
+ax1R.set_xticklabels(tick_function(arrayenua))
+ax1R.set_xlabel(r"Temps (fs)")
+
+
+plt.show()
+plt.close()
+pop1=np.genfromtxt('CSF_Q.dat')
+ax1=plt.subplot(1, 1, 1)
+plt.plot(champ[:,0],pop1[:],"b-")
+plt.xlabel(xlabeltime)
+plt.ylabel(ylabelpop)
+
+
+ax2 = ax1.twinx()
+ax2.plot(champ[:,0],champ[:,2],"r--")
+ax2.set_ylabel(r'$\vec{E}$ (u.a.)',fontsize=16)
+ax1R = ax1.twiny()
+arrayenfs=np.arange(0,20,5)
+print(arrayenfs)
+arrayenua=fs2au(arrayenfs)
+print(arrayenua)
+new_tick_locations=arrayenfs
+new_tick_locations=np.array([200])
+ax1R.set_xlim(ax1.get_xlim())
+ax1R.set_xticks(arrayenua)
+ax1R.set_xticklabels(tick_function(arrayenua))
+ax1R.set_xlabel(r"Temps (fs)",fontsize=16)
+ax1.set_ylabel(ylabelchamp)
+plt.gcf().subplots_adjust(top=0.85,bottom=0.15,left=0.1,right=0.8)
+
+plt.savefig('HCN_CSFQ.png')
+
+
+plt.show()
+plt.close()
+import os
+nbins=25
+can=1
+while(os.path.isfile('spectre2dXZ_canal0000'+str(can)+'.dat')):
+  if(can<10):
+    canal1_2d=np.loadtxt('spectre2dXZ_canal0000'+str(can)+'.dat')
+  else:
+    canal1_2d=np.loadtxt('spectre2dXZ_canal000'+str(can)+'.dat')
+  canal1_2d=canal1_2d.transpose()
+  fig4a = plt.figure()
+  fig4a.canvas.set_window_title('spectre canal '+str(can))
+  X=np.loadtxt('kx.dat')
+  X=X.transpose()
+  dx=X[0,1]-X[0,0]
+  dx=X[1,0]-X[0,0]
+  print("dx=",dx)
+  Y=np.loadtxt('kzx.dat')
+  Y=Y.transpose()
+  dy=Y[1,0]-Y[0,0]
+  print("dy=",dy)
+  leveltot = plt.MaxNLocator(nbins=nbins).tick_values(canal1_2d.min(), canal1_2d.max())
+  cf=plt.contourf(Y,X ,canal1_2d,levels=leveltot,cmap='jet')
+
+  xmin=-2
+  xmax=2.0
+  zmin=-2
+  zmax=2
+  plt.axis([xmin, xmax, zmin, zmax])
+  fig4a.colorbar(cf)
+  FILESPECTRE2Dcanal1='HCN_spectre2dcanal'+str(can)+'.pdf'
+  plt.ylabel('Momentum x (a.u.)',fontsize=16)
+  plt.xlabel('Momentum z (a.u.)',fontsize=16)
+  plt.savefig(FILESPECTRE2Dcanal1)
+  plt.show()
+  plt.close()
+  
+  # ZOOM
+  xmin=0
+  xmax=1.25
+  zmin=0
+  zmax=1.25
+  leveltot = plt.MaxNLocator(nbins=nbins).tick_values(canal1_2d.min(), canal1_2d.max())
+  cf=plt.contourf(Y,X ,canal1_2d,levels=leveltot,cmap='jet')
+
+
+  plt.axis([xmin, xmax, zmin, zmax])
+  fig4a.colorbar(cf)
+  FILESPECTRE2Dcanal1='HCN_spectre2dcanal_zoom'+str(can)+'.pdf'
+  plt.ylabel('Momentum x (a.u.)',fontsize=16)
+  plt.xlabel('Momentum z (a.u.)',fontsize=16)
+  plt.savefig(FILESPECTRE2Dcanal1)
+  plt.show()
+  plt.close()   
+    
+  fig4b = plt.figure()
+  fig4b.canvas.set_window_title('spectre canal '+str(can)+' LOG')
+  Z=np.log10(canal1_2d)
+  #-18 a -25
+  leveltot = plt.MaxNLocator(nbins=nbins).tick_values(Z.min(), Z.max())
+  cfb=plt.contourf(Y,X ,Z,levels=leveltot,cmap='jet')
+  LOGYMAX=5
+  LOGZMAX=5
+  plt.axis([-LOGYMAX, LOGYMAX, -LOGZMAX, LOGZMAX])
+  fig4b.colorbar(cfb)
+  FILESPECTRE2Dcanal1LOG='HCN_spectre2dcanal'+str(can)+'LOG.pdf'
+  plt.ylabel('Momentum x (a.u.)',fontsize=16)
+  plt.xlabel('Momentum z (a.u.)',fontsize=16)
+  plt.savefig(FILESPECTRE2Dcanal1LOG)
+  plt.show()
+  plt.close()
+
+  can=can+1
+    
+print('finish')
+
--- tools/orb.py
+++ tools/orb.py
@@ -1,42 +1,11 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-
-class molecule():
-    def __init__(self, atoms = [], totchg = 0., mocoeff = []):
-        self.atoms = atoms
-        self.totchg = totchg
-        self.mocoeff = mocoeff
-
-    def add_atoms(self,atom):
-        self.atoms.append(atom)
-
-
-class basis():
-    def __init__(self, listecons, l):
-        self.listecons = listecons
-        self.l = l
-
-
-class atom():
-    def __init__(self,   basis = [], pos=[0.,0.,0.], chg=1.0, type='H'):
-        self.type = type
-        self.chg = chg
-        self.pos = pos
-        self.basis = basis
-
-    def add_basis(self,basis):
-        self.basis.append(basis)
-
-REP = "../input/CO2/800nm_I5E13_NX_200_1MO_2e/BASIS/CO2/1/"
 xmax=6
 zmax=6
-# IDEE : comp[iler le read argos avec f2py
-#TODO réécrire sous forme matricielle
-x=np.arange(-xmax,xmax,0.01)
-z=np.arange(-zmax,zmax,0.01)
-
-orbc1s_mod = np.zeros((len(x),len(z)))
+x=np.arange(-xmax,xmax,0.001)
+z=np.arange(-zmax,zmax,0.001)
 orbc1s=np.zeros((len(x),len(z)))
 orbc2s=np.zeros((len(x),len(z)))
 orbc3s=np.zeros((len(x),len(z)))
@@ -73,7 +42,7 @@ MO2=np.zeros((len(x),len(z)))
 MO=np.zeros((27,len(x),len(z)))
 mocoeff=np.zeros((27,27))
 
-fname=REP+'mocoef_scf.sp'
+fname='mocoef_scf.sp'
 with open(fname) as f:
     content = f.readlines()
 content = [x.strip() for x in content]
@@ -88,397 +57,85 @@ while morb<27:
   ncont=ncont+1
  morb=morb+1
  norb=0
+#pos=1
+#dpos=19
+#mocoeff[0][0]=content[10][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][1]=content[10][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][2]=content[10][pos:pos+dpos]
+#pos=pos+dpos
+#print(content[10][pos:])
+#mocoeff[0][3]=content[10][pos:]
 
+#pos=1
+#mocoeff[0][4]=content[11][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][5]=content[11][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][6]=content[11][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][7]=content[11][pos:]
+#
+#pos=1
+#mocoeff[0][8]=content[12][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][9]=content[12][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][10]=content[12][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][11]=content[12][pos:]
+#
+#pos=1
+#mocoeff[0][12]=content[13][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][13]=content[13][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][14]=content[13][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][15]=content[13][pos:]
+#
+#pos=1
+#mocoeff[0][16]=content[14][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][17]=content[14][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][18]=content[14][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][dpos]=content[14][pos:]
+#pos=pos+dpos
+#
+#pos=1
+#mocoeff[0][20]=content[15][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][21]=content[15][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][22]=content[15][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][23]=content[15][pos:]
+#pos=pos+dpos
+#
+#pos=1
+#mocoeff[0][24]=content[16][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][25]=content[16][pos:pos+dpos]
+#pos=pos+dpos
+#mocoeff[0][26]=content[16][pos:]
 
 # TODO read the argosls as defined here :
 #https://www.univie.ac.at/columbus/docs_COL70/documentation_main.html
-# *******************************************************************************
-#
-#                                    INPUT DATA
-#
-# *******************************************************************************
-#
-#   The input is list directed except where a FORMAT statement is given.  It is
-#   recommended that a / be put at the end of each input record, as is done on
-#   line 2 and many other lines in the sample input data sets, in order to
-#   allow for future extensions.
-#
-#   1)     TITLE(1)
-#          FORMAT(A80)
-#
-#   2)     NGEN, NS, NAORDS, NCONS, NGCS, ITOL, ICUT, AOINTS, ONLY1E,
-#          INRM, NCRS, L1REC, L2REC, AOINT2, FSPLIT
-#
-#   3)     NST, (ND(I), ITYP(I), I = 1, NST)
-#          FORMAT(I3,12(I3,A3))
-#
-#   4)     NDPT
-#
-#   5)     DO I = 1, NDPT
-#            P1(I), P2(I), P3(I)
-#          ENDDO
-#
-#   6)     DO I = 1, NAORDS
-#            NREP(I), (IREP(J), J = 1, NREP(I))
-#          ENDDO
-#
-#   7)     DO I = 1, NGCS
-#            ICSU(I), ICTU(I), IAORDS(I)
-#            DO J = 1, ICSU(I)
-#              (ISOCOEF(K,J), K = 1, ICTU(I))
-#            ENDDO
-#          ENDDO
-#
-#   8)     DO I = 1, NCONS
-#            ICONU(I), LMNP1(I), NRCR(I)
-#            DO J = 1, ICONU(I)
-#              ZET(J,I), (ETA(K,J,I), K = 1, NRCR(I))
-#            ENDDO
-#          ENDDO
-#
-#   9)     IF(NCRS .NE. 0) THEN
-#            DO ICRS = 1, NCRS
-#              LCRU, LLSU
-#              IF(LCRU .GE. 0) THEN
-#                DO L = 0, LCRU
-#                  NBFCR
-#                  DO K = 1, NBFCR
-#                    NCR(K), ZCR(K), CCR(K)
-#                  ENDDO
-#                ENDDO
-#              ENDIF
-#              IF(LLSU .GE. 1) THEN
-#                DO L = 1, LLSU
-#                  NBFCR
-#                  DO K = 1, NBFCR
-#                    NCR(K), ZCR(K), CCR(K)
-#                  ENDDO
-#                ENDDO
-#              ENDIF
-#            ENDDO
-#          ENDIF
-#
-#  10)     DO IS = 1, NS
-#            MTYPE(IS), NF(IS), NC(IS), CHG(IS)
-#            FORMAT(A3,2I3,F3.0)
-#            DO J = 1, NC
-#              X(J), Y(J), Z(J)
-#            ENDDO
-#            IF(NC(IS) .NE. 1) THEN
-#              DO J = 1, NGEN
-#                (IGEN(K,J), K = 1, NC)
-#              ENDDO
-#            ENDIF
-#            DO J = 1, NF
-#              MCONS(J), IGCS(J)
-#            ENDDO
-#            IF(NCRS .NE. 0) THEN
-#              MCRS(IS)
-#            ENDIF
-#          ENDDO
-#
-#  11)     DO I = 2, 4
-#            TITLE(I)
-#            IF(TITLE(I) .EQ. ' ') EXIT
-#          ENDDO
-f = open(REP+'argosls.sp', "r")
-argosls = f.readlines()
-# title = argosls[2]
-#print(str(mocoeff[1]))
-#print("!!!!!!!!!!!!! TTTTESSSSSSSSSSSSTTTTTTT")
-#print(argosls[3].split())
-NGEN, NS, NAORDS, NCONS, NGCS, ITOL, ICUT, AOINTS, ONLY1E, \
- INRM, NCRS, L1REC, L2REC, AOINT2, FSPLIT = argosls[3].split()
-#   2)     NGEN, NS, NAORDS, NCONS, NGCS, ITOL, ICUT, AOINTS, ONLY1E,
-#          INRM, NCRS, L1REC, L2REC, AOINT2, FSPLIT
-#print(NGEN)
-#   3)     NST, (ND(I), ITYP(I), I = 1, NST)
-#          FORMAT(I3,12(I3,A3))
-#
-#   4)     NDPT
-NST = int(argosls[4].split()[0])
-j=1
-#print("NST = ", NST)
-ND = []
-ITYPE = []
-for i in range(NST):
-    print("test",i)
-    ND.append(argosls[4].split()[j])
-    ITYPE.append(argosls[4].split()[j+1])
-NDPT = int(argosls[5].split()[0])
-# TODO : this if NPDT > 0
-#   5)     DO I = 1, NDPT
-#            P1(I), P2(I), P3(I)
-#          ENDDO
 
-
-#
-#   6)     DO I = 1, NAORDS
-#            NREP(I), (IREP(J), J = 1, NREP(I))
-#          ENDDO
-#
-NAORDS = int(NAORDS)
-line=6
-#print("NAORDS = ",NAORDS)
-NREP=[]
-IREP=[]
-k=0
-for i in range(NAORDS):
-    NREP.append(int(argosls[6+i].split()[0]))
-    for j in range(NREP[k]):
-        IREP.append(argosls[6+i].split()[j+1])
-    k=k+1
-line = line+NAORDS-1
-#   7)     DO I = 1, NGCS
-#            ICSU(I), ICTU(I), IAORDS(I)
-#            DO J = 1, ICSU(I)
-#              (ISOCOEF(K,J), K = 1, ICTU(I))
-#            ENDDO
-#          ENDDO
-NGCS = int(NGCS)
-ICSU = []
-ICTU = []
-IAORDS = []
-ISOCOEF = []
-k = 0
-
-for i in range(NGCS):
-    ICSU.append(int(argosls[line+k].split()[0]))
-    ICTU.append(int(argosls[line+k].split()[1]))
-    IAORDS.append(int(argosls[line+k].split()[2]))
-
-    for j in range(ICSU[k]):
-        for l in range(ICTU[k]):
-            ISOCOEF.append(argosls[line+k+1].split())
-            print(argosls[line+k+1].split())
-
-
-    print(line)
-print(NGCS+ICSU[0])
-
-line=line+NGCS+sum(ICSU)
-print("test line")
-print(line)
-print(NGCS)
-line = line - 1
-print(argosls[line].split())
-print(NCONS)
-ZET = []
-ETA = []
-ICONU = []
-LMNP1 = []
-NRCR = []
-NCONS=int(NCONS)
-
-liste_base = []
-
-
-
-
-
-for i in range(NCONS):
-    a,b,c = argosls[line].split()
-    ICONU.append(int(a))
-    LMNP1.append(int(b))
-    NRCR.append(int(c))
-    line = line + 1
-    print(a,b,c)
-    une_base1 = []
-    for j in range(int(a)):
-        ZET.append(argosls[line].split()[0])
-        ETA.append(argosls[line].split()[1])
-        #print()
-        une_base1.append([float(argosls[line].split()[0]),float(argosls[line].split()[1])])
-        #print("test base test base")
-        line=line+1
-
-    liste_base.append(une_base1)
-#print("testi")
-#print(liste_base)
-
-#print(liste_base[0])
-#print(ICSU)
-#line=line+ICSU[0]+ICSU[1]
-#print(argosls[line].split())
-#   8)     DO I = 1, NCONS
-#            ICONU(I), LMNP1(I), NRCR(I)
-#            DO J = 1, ICONU(I)
-#              ZET(J,I), (ETA(K,J,I), K = 1, NRCR(I))
-#            ENDDO
-#          ENDDO
-#
-#   9)     IF(NCRS .NE. 0) THEN
-#            DO ICRS = 1, NCRS
-#              LCRU, LLSU
-#              IF(LCRU .GE. 0) THEN
-#                DO L = 0, LCRU
-#                  NBFCR
-#                  DO K = 1, NBFCR
-#                    NCR(K), ZCR(K), CCR(K)
-#                  ENDDO
-#                ENDDO
-#              ENDIF
-#              IF(LLSU .GE. 1) THEN
-#                DO L = 1, LLSU
-#                  NBFCR
-#                  DO K = 1, NBFCR
-#                    NCR(K), ZCR(K), CCR(K)
-#                  ENDDO
-#                ENDDO
-#              ENDIF
-#            ENDDO
-#          ENDIF
-#
-NS = int(NS)
-#line=line+1
-MTYPE = []
-NF  = []
-NC = []
-CHG = np.zeros(NS)
-pos=[]
-print("testestestset")
-print(type(argosls[line].split()))
-MCONS = []
-IGCS = []
-molecule = molecule()
-atoms = []
-for i in range(NS):
-    temp = argosls[line].split()
-    print("test3")
-    print(temp)
-    MTYPE.append(temp[0])
-    NF.append(int(temp[1]))
-    print("testtestsetsettemp2")
-    print(temp[2])
-    NC.append(int(temp[2]))
-    CHG[i] = temp[3]
-    line = line+1
-    print(NC[i])
-
-    for j in range(NC[i]):
-
-        postemp = np.array(argosls[line].split()).transpose()
-        pos.append(postemp)
-        atoms.append(atom(type=temp[0],chg=CHG[i],pos=postemp))
-        #unatom =
-        print('testi',i)
-        molecule.add_atoms(atom(type=temp[0],chg=CHG[i],pos=postemp))
-        line=line+1
-        if (NC[i] != 1):
-            print("TODO: NC[1] != 1")
-        print("NF[",i,"]=",NF[i])
-        for k in range(NF[i]):
-            MCONS.append(argosls[line].split()[0])
-            IGCS.append(argosls[line].split()[1])
-            print("test base")
-            print(argosls[line].split()[0], argosls[line].split()[1])
-            print("Another Test Testing TestT")
-            print(int(argosls[line].split()[0])-1)
-            a = int(argosls[line].split()[0])-1
-            print(liste_base[a])
-            l = int(argosls[line].split()[1])
-            basenumber = int(argosls[line].split()[0])-1
-            print("l = ",'basenumber = ',basenumber)
-            unebase = basis(liste_base[basenumber] ,l)
-            print("Addinf  a base to atom["+str(i)+"]")
-            atoms[i].add_basis(unebase)
-            molecule.atoms[i].add_basis(unebase)
-            line = line +1
-
-    #molecule.add_atoms(unatom)
-        #            ENDDO
-        #            IF(NCRS .NE. 0) THEN
-        #              MCRS(IS)
-        #            ENDIF
-        #          ENDDO
-print("testsetst")
-print(pos[0])
-#  10)     DO IS = 1, NS
-#            MTYPE(IS), NF(IS), NC(IS), CHG(IS)
-#            FORMAT(A3,2I3,F3.0)
-#            DO J = 1, NC
-#              X(J), Y(J), Z(J)
-#            ENDDO
-#            IF(NC(IS) .NE. 1) THEN
-#              DO J = 1, NGEN
-#                (IGEN(K,J), K = 1, NC)
-#              ENDDO
-#            ENDIF
-#            DO J = 1, NF
-#              MCONS(J), IGCS(J)
-#            ENDDO
-#            IF(NCRS .NE. 0) THEN
-#              MCRS(IS)
-#            ENDIF
-#          ENDDO
-#
-#  11)     DO I = 2, 4
-#            TITLE(I)
-#            IF(TITLE(I) .EQ. ' ') EXIT
-#          ENDDO
-
-
-
+print(str(mocoeff[1]))
 
 gridX=np.zeros((len(x),len(z)))
 gridZ=np.zeros((len(x),len(z)))
 r=0
-
-
-x2 = np.zeros((len(x),len(z)))
-z2 = np.zeros((len(x),len(z)))
-# TODO : use this meshgrid and delete the loop
-xv, zv = np.meshgrid(x, z, sparse=False, indexing='ij')
-
-print("test molecule")
-print(molecule.atoms)
-n = 0
-i=0
-for atom in molecule.atoms:
-    print(atom.type)
-    print(atom.pos)
-    print("Len atom.basis " + str(len(atom.basis)))
-    print("Len atoms[i].basis " + str(len(atoms[n].basis)))
-    i=i+1
-    for unebase in atom.basis:
-        print('listecons = ',unebase.listecons)
-        print('l = ',unebase.l)
-        if unebase.l == 1:
-            print("fonction 1s")
-            orb = np.zeros(xv.shape)
-            f = open('orb_' + str(n) + '.txt', 'w')
-            for con in unebase.listecons:
-                f.write(str(con[0])+" "+str(con[1])+"\n")
-                orb = orb + con[1] * np.exp(-con[0] * (xv ** 2.0 + zv ** 2.0))
-            f.close()
-            plt.contourf(xv, zv, orb)
-            plt.savefig('orb_'+str(n)+'.png')
-            n = n + 1
-#orbc1s_mod2 = 1.8347002E-03 * np.exp(-3047.525 * (x2 ** 2.0 + z2 ** 2.0)) \
-#                  + 1.4037301E-02 * np.exp(-457.3695 * (x2 ** 2.0 + z2 ** 2.0)) \
-#                  + 6.8842606E-02 * np.exp(-103.9487 * (x2 ** 2.0 + z2 ** 2.0)) \
-#                  + 0.2321844 * np.exp(-29.21016 * (x2 ** 2.0 + z2 ** 2.0)) \
-#                  + 0.4679413 * np.exp(-9.286663 * (x2 ** 2.0 + z2 ** 2.0)) \
-#                  + 0.3623120 * np.exp(-3.163927 * (x2 ** 2.0 + z2 ** 2.0))
-
-    #print(atom.basis)
-
-
 for nx in range(len(x)):
     for nz in range(len(z)):
-        x2[nx, nz] = x[nx]
-        z2[nx, nz] = z[nz]
         r=np.sqrt(x[nx]**2.0+z[nz]**2.0)
         gridX[nx,nz]=x[nx]
         gridZ[nx,nz]=z[nz]
-        #orbc1s[nx,nz]=1.8347002E-03*np.exp(-3047.525*r**2)+1.4037301E-02*np.exp(-457.3695*r**2)+6.8842606E-02*np.exp(-103.9487*r**2)+0.2321844*np.exp(-29.21016*r**2)+0.4679413*np.exp(-9.286663*r**2)+0.3623120*np.exp(-3.163927*r**2)
-        #orbc1s_mod[nx, nz] = 1.8347002E-03 * np.exp(-3047.525 * (x[nx]**2.0+z[nz]**2.0)) \
-                             #+ 1.4037301E-02 * np.exp(-457.3695 * (x[nx]**2.0+z[nz]**2.0)) \
-                             #+ 6.8842606E-02 * np.exp(-103.9487 * (x[nx]**2.0+z[nz]**2.0)) \
-                             #+ 0.2321844 * np.exp(-29.21016 * (x[nx]**2.0+z[nz]**2.0)) \
-                             #+ 0.4679413 * np.exp(-9.286663 * (x[nx]**2.0+z[nz]**2.0)) \
-                             #+ 0.3623120 * np.exp(-3.163927 * (x[nx]**2.0+z[nz]**2.0))
-
+        orbc1s[nx,nz]=1.8347002E-03*np.exp(-3047.525*r**2)+1.4037301E-02*np.exp(-457.3695*r**2)+6.8842606E-02*np.exp(-103.9487*r**2)+0.2321844*np.exp(-29.21016*r**2)+0.4679413*np.exp(-9.286663*r**2)+0.3623120*np.exp(-3.163927*r**2)
         orbc2s[nx,nz]=-0.1193324*np.exp(-7.868272*r**2)-0.1608542*np.exp(-1.881288*r**2)+1.143456*np.exp(-0.5442493*r**2)
         orbc3s[nx,nz]=np.exp(-0.1687144*r**2)
         orbc2px[nx,nz]=6.8999096E-02*np.exp(-7.868272*r**2)+0.3164240*np.exp(-1.881288*r**2)+0.7443083*np.exp(-0.5442493*r**2)*x[nx]
@@ -527,44 +184,34 @@ for nx in range(len(x)):
         mocoeff[0][25]*orbO2_3py[nx,nz]+\
         mocoeff[0][26]*orbO2_3pz[nx,nz]
 
-
         for n in range(27):
-          MO[n,nx,nz]=mocoeff[n][0]*orbc1s[nx,nz]+mocoeff[n][1]*orbc2s[nx,nz]+mocoeff[n][2]*orbc3s[nx,nz]+mocoeff[n][3]*orbc2px[nx,nz]+mocoeff[n][4]*orbc2py[nx,nz]+mocoeff[n][5]*orbc2pz[nx,nz]+mocoeff[n][6]*orbc3px[nx,nz]+\
-          mocoeff[n][7]*orbc3py[nx,nz]+\
-          mocoeff[n][8]*orbc3pz[nx,nz]+\
-          mocoeff[n][9]*orbO1_1s[nx,nz]+\
-          mocoeff[n][10]*orbO1_2s[nx,nz]+\
-          mocoeff[n][11]*orbO1_3s[nx,nz]+\
-          mocoeff[n][12]*orbO1_2px[nx,nz]+\
-          mocoeff[n][13]*orbO1_2py[nx,nz]+\
-          mocoeff[n][14]*orbO1_2pz[nx,nz]+\
-          mocoeff[n][15]*orbO1_3px[nx,nz]+\
-          mocoeff[n][16]*orbO1_3py[nx,nz]+\
-          mocoeff[n][17]*orbO1_3pz[nx,nz]+\
-          mocoeff[n][18]*orbO2_1s[nx,nz]+\
-          mocoeff[n][19]*orbO2_2s[nx,nz]+\
-          mocoeff[n][20]*orbO2_3s[nx,nz]+\
-          mocoeff[n][21]*orbO2_2px[nx,nz]+\
-          mocoeff[n][22]*orbO2_2py[nx,nz]+\
-          mocoeff[n][23]*orbO2_2pz[nx,nz]+\
-          mocoeff[n][24]*orbO2_3px[nx,nz]+\
-          mocoeff[n][25]*orbO2_3py[nx,nz]+\
-          mocoeff[n][26]*orbO2_3pz[nx,nz]
+            MO[n,nx,nz]=mocoeff[n][0]*orbc1s[nx,nz]+mocoeff[n][1]*orbc2s[nx,nz]+mocoeff[n][2]*orbc3s[nx,nz]+mocoeff[n][3]*orbc2px[nx,nz]+mocoeff[n][4]*orbc2py[nx,nz]+mocoeff[n][5]*orbc2pz[nx,nz]+mocoeff[n][6]*orbc3px[nx,nz]+\
+            mocoeff[n][7]*orbc3py[nx,nz]+\
+            mocoeff[n][8]*orbc3pz[nx,nz]+\
+            mocoeff[n][9]*orbO1_1s[nx,nz]+\
+            mocoeff[n][10]*orbO1_2s[nx,nz]+\
+            mocoeff[n][11]*orbO1_3s[nx,nz]+\
+            mocoeff[n][12]*orbO1_2px[nx,nz]+\
+            mocoeff[n][13]*orbO1_2py[nx,nz]+\
+            mocoeff[n][14]*orbO1_2pz[nx,nz]+\
+            mocoeff[n][15]*orbO1_3px[nx,nz]+\
+            mocoeff[n][16]*orbO1_3py[nx,nz]+\
+            mocoeff[n][17]*orbO1_3pz[nx,nz]+\
+            mocoeff[n][18]*orbO2_1s[nx,nz]+\
+            mocoeff[n][19]*orbO2_2s[nx,nz]+\
+            mocoeff[n][20]*orbO2_3s[nx,nz]+\
+            mocoeff[n][21]*orbO2_2px[nx,nz]+\
+            mocoeff[n][22]*orbO2_2py[nx,nz]+\
+            mocoeff[n][23]*orbO2_2pz[nx,nz]+\
+            mocoeff[n][24]*orbO2_3px[nx,nz]+\
+            mocoeff[n][25]*orbO2_3py[nx,nz]+\
+            mocoeff[n][26]*orbO2_3pz[nx,nz]
 
-orbc1s_mod2 = 1.8347002E-03 * np.exp(-3047.525 * (x2**2.0+z2**2.0)) \
-                             + 1.4037301E-02 * np.exp(-457.3695 * (x2**2.0+z2**2.0)) \
-                             + 6.8842606E-02 * np.exp(-103.9487 * (x2**2.0+z2**2.0)) \
-                             + 0.2321844 * np.exp(-29.21016 * (x2**2.0+z2**2.0)) \
-                             + 0.4679413 * np.exp(-9.286663 * (x2**2.0+z2**2.0)) \
-                             + 0.3623120 * np.exp(-3.163927 * (x2**2.0+z2**2.0))
+
 
 #*np.exp**(*r**2)
 plt.contourf(gridX,gridZ ,orbc1s)
 plt.savefig('orbc1s.png')
-plt.contourf(gridX,gridZ ,orbc1s_mod)
-plt.savefig('orbc1s_mod.png')
-plt.contourf(gridX,gridZ ,orbc1s_mod2)
-plt.savefig('orbc1s_mod2.png')
 plt.contourf(gridX,gridZ ,orbc2s)
 plt.savefig('orbc2s.png')
 plt.contourf(gridX,gridZ ,orbc3s)
@@ -612,4 +259,3 @@ plt.savefig('orbMO2.png')
 for n in range(27):
     plt.contourf(gridX,gridZ ,MO[n,:,:])
     plt.savefig('orbMO'+str(n)+'.png')
-
